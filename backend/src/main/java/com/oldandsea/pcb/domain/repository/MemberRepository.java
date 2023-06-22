0c274f7ca2f8bb1511b03bda45eedc6bbeca36a1
Jin Hyuk <ekrrdj07@gmail.com>
2023-06-22 22:11:57 +0900

    remove file

--- backend/src/main/java/com/oldandsea/pcb/domain/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oldandsea.pcb.domain.repository;
-import com.oldandsea.pcb.domain.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByNickname(String nickname);
-}
--- backend/src/main/java/com/oldandsea/pcb/domain/repository/boardrepository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oldandsea.pcb.domain.repository.boardrepository;
-import com.oldandsea.pcb.domain.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-import java.util.List;
-
-public interface BoardRepository extends JpaRepository<Board, Long>, BoardRepositoryCustom {
-}
